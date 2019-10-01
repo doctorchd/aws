871173d914b74db41cf5ed72c07aa2a907bde8ed
Dmytro Cherkasov <dmytrocherkasov@fwdext.com>
2019-10-01 18:22:56 +0300

    create.sh: added --capabilities CAPABILITY_NAMED_IAM

--- aws-essentials/create.sh
+++ aws-essentials/create.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-aws cloudformation create-stack --stack-name TestVPC --template-body file://VPC.json
+aws cloudformation create-stack \
+    --stack-name TestVPC \
+    --template-body file://VPC.json \
+    --capabilities CAPABILITY_NAMED_IAM
