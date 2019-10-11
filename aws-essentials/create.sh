7ef313da34a26642ead0210aebfc4c1deaf3bd43
Dmytro Cherkasov <dmytrocherkasov@fwdext.com>
2019-10-11 21:31:58 +0300

    add VPC_light, update create, test UserData and balancer

--- aws-essentials/create.sh
+++ aws-essentials/create.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+if [ "XXX$1" = "XXX" ]; then
+    echo "Add VPC name as parameter"
+    echo
+    exit 1
+fi
+
+TMPL="$1"
+
+if [ ! -f $TMPL ]; then
+    echo "File $TMPL does not exist"
+    exit 1
+fi
+
 aws cloudformation create-stack \
     --stack-name TestVPC \
-    --template-body file://VPC.json \
+    --template-body file://$TMPL \
     --capabilities CAPABILITY_NAMED_IAM
+
